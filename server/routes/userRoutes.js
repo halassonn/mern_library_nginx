619e30ba961bbe4594dd88510036137137619084
halassonn <gtm24halas@gmail.com>
2021-09-21 17:01:15 +0700

    add nginx dan app client penggajian

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   userRoutes.js                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halasson <halasson@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/21 15:07:31 by halasson          #+#    #+#             */
+/*   Updated: 2021/09/21 15:07:32 by halasson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 const { Login, Logout } = require("../controllers/userController");
 const router = require("express-promise-router")();
 
